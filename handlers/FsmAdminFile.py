5851e7d3bebfa3234b501c383dbcd807ade050f4
Ismar <ismar110501@gmail.com>
2023-09-01 15:02:11 +0600

    FSMAdmin

--- handlers/FsmAdminFile.py
+++ handlers/FsmAdminFile.py
@@ -12,6 +12,7 @@ class FSMAdmin(StatesGroup):
     group = State()
     problem = State()
     numberhw = State()
+    submit = State()
 
 
 async def fsm_start(message: types.Message):
@@ -22,13 +23,62 @@ async def fsm_start(message: types.Message):
     else:
         await message.answer('Пиши в личку!')
 
+
 async def load_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['name'] = message.from_user.username
-        print(data)
+        data['name'] = message.text
+    await FSMAdmin.next()
+    await message.answer('Какой у вас направление?')
 
 
+async def load_direction(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['direction'] = message.text
+    await FSMAdmin.next()
+    await message.answer('В какой вы группе?\n'
+                         'пример: \n'
+                         '22.1, 33.2, 39.3')
 
+
+async def load_group(message: types.Message, state: FSMContext):
+    try:
+        if not 20 < float(message.text) < 40:
+            await message.answer('такой группы не существует')
+            await state.finish()
+            return
+        async with state.proxy() as data:
+            data['group'] = message.text
+        await FSMAdmin.next()
+        await message.answer('какие у вас проблемаы? попытайтесь описать его')
+    except:
+        await message.answer('пишите как было в примере')
+
+
+async def load_problem(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['problem'] = message.text
+
+    await FSMAdmin.next()
+    await message.answer('Какой это по счёту домашняя работа?')
+async def load_numberhw(message: types.Message, state: FSMContext):
+    async  with state.proxy() as data:
+        data['numberhw'] = message.text
+        await bot.send_message(f'{data["name"]}, {data["direction"]}, {data["group"]}\n'
+                               f'{data["problem"]}, {data["numberhw"]}')
+    await FSMAdmin.next()
+    await message.answer('Всё верно?')
+
+async def submit(message: types.Message, state: FSMContext):
+    if message.text.lower() == 'да':
+        pass
+    elif message.text.lower() == 'нет':
+        await state.finish()
+        return
 def register_handlers_fsm_anketa(dp: Dispatcher):
     dp.register_message_handler(fsm_start, commands=['helpmentors'])
-    dp.register_message_handler(load_name, state=FSMAdmin.name, content_types=['text'])
+    dp.register_message_handler(load_name, state=FSMAdmin.name)
+    dp.register_message_handler(load_direction, state=FSMAdmin.direction)
+    dp.register_message_handler(load_group, state=FSMAdmin.group)
+    dp.register_message_handler(load_problem, state=FSMAdmin.problem)
+    dp.register_message_handler(load_numberhw, state=FSMAdmin.numberhw)
+    dp.register_message_handler(submit, state=FSMAdmin.submit)
