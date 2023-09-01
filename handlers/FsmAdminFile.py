3cda678e2ef77632dec259eb3f8421d2dcc719c4
Ismar <ismar110501@gmail.com>
2023-09-01 16:19:07 +0600

    finished FSMAdmin

--- handlers/FsmAdminFile.py
+++ handlers/FsmAdminFile.py
@@ -60,21 +60,38 @@ async def load_problem(message: types.Message, state: FSMContext):
 
     await FSMAdmin.next()
     await message.answer('Какой это по счёту домашняя работа?')
+
+
 async def load_numberhw(message: types.Message, state: FSMContext):
-    async  with state.proxy() as data:
+    async with state.proxy() as data:
         data['numberhw'] = message.text
-        await bot.send_message(f'{data["name"]}, {data["direction"]}, {data["group"]}\n'
-                               f'{data["problem"]}, {data["numberhw"]}')
+        await message.answer(f'{data["name"]}, {data["direction"]}, {data["group"]}\n'
+                             f'{data["problem"]}, {data["numberhw"]}')
     await FSMAdmin.next()
     await message.answer('Всё верно?')
 
+
+async def cancel_help(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is not None:
+        await state.finish()
+        await message.answer('отменено')
+
+
 async def submit(message: types.Message, state: FSMContext):
     if message.text.lower() == 'да':
-        pass
+        await state.finish()
+        await message.answer('Сообщение было отправленно Старшему Ментору вашего направление\n'
+                             'Скоро вам напишут ментор')
     elif message.text.lower() == 'нет':
         await state.finish()
-        return
+        await message.answer('Отменено')
+    else:
+        await message.answer('Не пон')
+
+
 def register_handlers_fsm_anketa(dp: Dispatcher):
+    dp.register_message_handler(cancel_help, state='*', commands=['cancel'])
     dp.register_message_handler(fsm_start, commands=['helpmentors'])
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_direction, state=FSMAdmin.direction)
