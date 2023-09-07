19d03411f62f181b69500b161ad770eaddd3df99
Ismar <ismar110501@gmail.com>
2023-09-07 13:16:04 +0600

    admin command and filter swear

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import logging
 from handlers import client, callback, extra, admin, FsmAdminFile, notifications, inline
 from database.bot_db import sql_create
 
-
 # async def on_startup(_):
 #     # asyncio.create_task(notifications.scheduler())
 #     sql_create()
 #
+extra.register_handlers_extra(dp)
+admin.register_handlers_admin(dp)
 client.register_handlers_client(dp)
 FsmAdminFile.register_handlers_fsm_anketa(dp)
 
